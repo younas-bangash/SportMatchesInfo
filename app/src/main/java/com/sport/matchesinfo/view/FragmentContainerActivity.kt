4bd49b8463590d4412edff0303a8922a687cd6ab
muhammadyounas <muhammad.younas@nagarro.com>
2021-07-27 08:59:26 +0400

    Initial commit - Initial Project Setup

---
+++ app/src/main/java/com/sport/matchesinfo/view/FragmentContainerActivity.kt
@@ -0,0 +1,12 @@
+package com.sport.matchesinfo.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sport.matchesinfo.R
+
+class FragmentContainerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
